70e939458d0236767aa500eec92c6d5da2694466
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2023-11-20 15:02:36 +0530

    Add files via upload

---
+++ validation/java/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.validation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+public class MainActivity extends AppCompatActivity {
+    Button bCancel, bProceed;
+    EditText etFirstName, etLastName, etEmail, etPassword;
+    boolean isAllFieldsChecked = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bProceed = findViewById(R.id.proceedButton);
+        bCancel = findViewById(R.id.cancelButton);
+
+        // register all the EditText fields with their IDs.
+        etFirstName = findViewById(R.id.firstName);
+        etLastName = findViewById(R.id.lastName);
+        etEmail = findViewById(R.id.email);
+        etPassword = findViewById(R.id.password);
+
+        bProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // store the returned value of the dedicated function which checks
+                // whether the entered data is valid or if any fields are left blank.
+                isAllFieldsChecked = CheckAllFields();
+
+                // the boolean variable turns to be true then
+                // only the user must be proceed to the activity2
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+        // if user presses the cancel button then close the
+        // application or the particular activity.
+        bCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.finish();
+                System.exit(0);
+            }
+        });
+    }
+
+    // function which checks all the text fields
+    // are filled or not by the user.
+    // when user clicks on the PROCEED button
+    // this function is triggered.
+    private boolean CheckAllFields() {
+        if (etFirstName.length() == 0) {
+            etFirstName.setError("This field is required");
+            return false;
+        }
+
+        if (etLastName.length() == 0) {
+            etLastName.setError("This field is required");
+            return false;
+        }
+
+        if (etEmail.length() == 0) {
+            etEmail.setError("Email is required");
+            return false;
+        }
+
+        if (etPassword.length() == 0) {
+            etPassword.setError("Password is required");
+            return false;
+        } else if (etPassword.length() < 8) {
+            etPassword.setError("Password must be minimum 8 characters");
+            return false;
+        }
+
+        // after all validation return true.
+        return true;
+    }
+}
+
